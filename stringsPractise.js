6ad13521357f927771ec892c5de9ea2e917e91c9
BhumikaAggarwal <aggarwalbhumika99@gmail.com>
2022-12-27 20:15:37 +0530

    code of arrays started

--- stringsPractise.js
+++ stringsPractise.js
@@ -2,4 +2,14 @@ let nam = "Dolly"
 let n = nam.length
 for(let i = 0 ; i+1 <= n ; i++){
     console.log(nam[i])
-}
+}   
+console.log(nam.startsWith('D'))
+
+//Extracting a number from the string and converting it into a actual string.
+let sentence = "Please give rupees 1000"
+let amount = Number.parseInt(sentence.slice("please give rupees".length ))
+console.log(amount)
+// strings are immutable . An original string can never be changed.
+
+let newNam = nam.replace(nam[3] , 'p')
+console.log(newNam)
